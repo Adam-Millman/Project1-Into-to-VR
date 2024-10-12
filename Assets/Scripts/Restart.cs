de48f3c2d6cb35f4fc950c57638138157ae2568d
Adam-Millman <amillman20@gmail.com>
2024-10-11 20:34:07 -0600

    final build

---
+++ Assets/Scripts/Restart.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class Restart : MonoBehaviour
+{
+    public InputActionReference triggerLeft;
+    public InputActionReference triggerRight;
+    
+    void Update()
+    {
+        float leftTriggerValue = triggerLeft.action.ReadValue<float>();
+        float rightTriggerValue = triggerLeft.action.ReadValue<float>();
+
+        if(leftTriggerValue > 0.5f && rightTriggerValue > 0.5f){
+            SceneManager.LoadScene("Scenes/Intro");
+        }
+    }
+}
